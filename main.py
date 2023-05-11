a567a5c784ebd5fb35fcf7a54db7fc336331fcb3
sinhyunsung <hyunsungsung@naver.com>
2023-05-11 16:16:14 +0900

    update: 데이터 50개 이상 링크 댓글 크롤링 추가

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def find_iframe_name(driver, name, second):
         try:
             iframe_list = driver.find_elements(by=By.TAG_NAME, value="iframe")
             for i in iframe_list:
-                if name in i.get_attribute("title"):
+                if name in i.get_attribute("title") or "�쇱씠釉뚮━ - �볤��곸뿭" in i.get_attribute("title"):
                     conf=1
         except:
             continue
@@ -169,7 +169,29 @@ link_function_dic={
     "www.gnnews.co.kr":"scrap_gn",
     "www.hani.co.kr":"scrap_hani",
     "www.hankyung.com":"scrap_hankyung",
-
+    "www.idomin.com":"scrap_idomin",
+    "www.inven.co.kr":"scrap_inven",
+    "www.jemin.com":"scrap_jemin",
+    "www.jeonmae.co.kr":"scrap_jeonmae",
+    "www.jjan.kr":"scrap_jjan",
+    "www.joongang.co.kr":"scrap_joongang",
+    "www.joongboo.com":"scrap_sbs",
+    "www.kado.net":"scrap_kado",
+    "www.khan.co.kr":"scrap_sbs",
+    "www.kjilbo.co.kr":"scrap_kado",
+    "www.kookje.co.kr":"scrap_sbs",
+    "www.korea.kr":"scrap_korea",
+    "www.ksilbo.co.kr":"scrap_ksilbo",
+    "www.kwnews.co.kr":"scrap_sbs",
+    "www.kyeongin.com":"scrap_sbs",
+    "www.mk.co.kr":"scrap_mk",
+    "www.munhwa.com":"scrap_sbs",
+    "www.ppomppu.co.kr":"scrap_ppomppu",
+    "www.sedaily.com":"scrap_sedaily",
+    "www.seoul.co.kr":"scrap_seoul",
+    "www.slrclub.com":"scrap_slr",
+    "www.todayhumor.co.kr":"scrap_humor",
+    "www.yeongnam.com":"scrap_sbs",
 
 }
 
@@ -195,7 +217,7 @@ def scrap_kmib(url):
 
             driver.switch_to.frame(i)
             while(1):
-                driver_wait(driver, By.CLASS_NAME, "reply-content")
+                # driver_wait(driver, By.CLASS_NAME, "reply-content")
                 time.sleep(1)
                 temp = driver.find_elements(by=By.CLASS_NAME, value="reply-comment-btn")
                 for j in temp:
@@ -273,6 +295,26 @@ def scrap_cctoday(url):
     comment_list = list(map(lambda x:x.text,driver.find_elements(by=By.CLASS_NAME, value="comments-content")))
     return comment_list
 
+def scrap_jemin(url):
+    driver.get(url=url)
+    try:
+        driver_button_wait(driver, By.CLASS_NAME, "reply-more")
+        more_btn = driver.find_element(by=By.CLASS_NAME, value="reply-more")
+        more_btn.send_keys(Keys.ENTER)
+    except:
+        pass
+
+    comment_list=[]
+    while(1):
+        try:
+            comment_list += list(map(lambda x:x.text,driver.find_elements(by=By.CLASS_NAME, value="comments-content")))
+            paging=driver.find_element(by=By.CLASS_NAME, value="pagination")
+            atag=paging.find_elements(by=By.TAG_NAME, value="a")[1]
+            atag.click()
+        except:
+            break
+    return comment_list
+
 def scrap_ruli(url):
     driver.get(url=url)
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -287,11 +329,44 @@ def scrap_dc(url):
     comment_list = list(map(lambda x:x.text,comment_tag.find_elements(by=By.CLASS_NAME, value="txt")))[:-1]
     return comment_list
 
+def scrap_ppomppu(url):
+    driver.get(url=url)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    comment_tag=driver.find_element(by=By.ID, value="quote")
+    comment_list = list(map(lambda x:x.text,comment_tag.find_elements(by=By.CLASS_NAME, value="mid-text-area")))
+    return comment_list
+
 def scrap_clien(url):
     driver.get(url=url)
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     comment_tag=driver.find_element(by=By.CLASS_NAME, value="post_comment")
     comment_list = list(map(lambda x:x.text,comment_tag.find_elements(by=By.CLASS_NAME, value="comment_view")))
+    return comment_list
+
+def scrap_slr(url):
+    driver.get(url=url)
+    comment_tag=driver.find_element(by=By.CLASS_NAME, value="list")
+    comment_list = list(map(lambda x:x.text,comment_tag.find_elements(by=By.CLASS_NAME, value="cmt-contents")))
+    return comment_list
+
+def scrap_humor(url):
+    driver.get(url=url)
+    comment_tag=driver.find_element(by=By.ID, value="memoContainerDiv")
+    comment_list = list(map(lambda x:x.text,comment_tag.find_elements(by=By.CLASS_NAME, value="memoContent")))
+    return comment_list
+
+def scrap_joongang(url):
+    driver.get(url=url)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    comment_tag=driver.find_element(by=By.CLASS_NAME, value="comment_wrap")
+    comment_list = list(map(lambda x:x.text,comment_tag.find_elements(by=By.CLASS_NAME, value="comment_text")))
+    return comment_list
+
+def scrap_inven(url):
+    driver.get(url=url)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    comment_tag=driver.find_element(by=By.ID, value="powerbbsCmt2")
+    comment_list = list(map(lambda x:x.text,comment_tag.find_elements(by=By.CLASS_NAME, value="cmtContentOne")))
     return comment_list
 
 def scrap_ddanzi(url):
@@ -329,6 +404,13 @@ def scrap_daejon(url):
     comment_list = list(map(lambda x:x.text,comment_tag.find_elements(by=By.CLASS_NAME, value="comment-content-view")))
     return comment_list
 
+def scrap_jeonmae(url):
+    driver.get(url=url)
+    driver_wait(driver, By.CLASS_NAME, "reply-list")
+    comment_tag=driver.find_element(by=By.CLASS_NAME, value="reply-list")
+    comment_list = list(map(lambda x:x.text,comment_tag.find_elements(by=By.CLASS_NAME, value="comments-content")))
+    return comment_list
+
 def scrap_hkib(url):
     driver.get(url=url)
     driver_wait(driver, By.CLASS_NAME, "comment-box")
@@ -336,11 +418,56 @@ def scrap_hkib(url):
     comment_list = list(map(lambda x:x.text,comment_tag.find_elements(by=By.CLASS_NAME, value="text")))
     return comment_list
 
+def scrap_ksilbo(url):
+    driver.get(url=url)
+    driver_wait(driver, By.ID, "comment-list")
+    comment_tag=driver.find_element(by=By.ID, value="comment-list")
+
+    more_btn=driver.find_elements(by=By.CLASS_NAME, value="btn-comment-sub")
+    for i in more_btn:
+        try:
+            if i.get_attribute("title")=="답글":
+                i.click()
+        except:
+            continue
+
+    comment_list = list(map(lambda x:x.text,comment_tag.find_elements(by=By.CLASS_NAME, value="comment-content-view")))
+    return comment_list
+
+def scrap_jjan(url):
+    driver.get(url=url)
+
+    comment_list=[]
+    while(1):
+        try:
+            comment_list += list(map(lambda x:x.text,driver.find_elements(by=By.CLASS_NAME, value="reply_txt")))
+            paging=driver.find_element(by=By.CLASS_NAME, value="pagination")
+            atag=paging.find_elements(by=By.TAG_NAME, value="a")[2]
+            atag.click()
+            time.sleep(1)
+        except:
+            break
+    return comment_list
+
 def scrap_fm(url):
     driver.get(url=url)
     driver_wait(driver, By.ID, "cmtPosition")
     comment_tag=driver.find_element(by=By.ID, value="cmtPosition")
     comment_list = list(map(lambda x:x.text,comment_tag.find_elements(by=By.CLASS_NAME, value="comment-content")))
+    return comment_list
+
+def scrap_idomin(url):
+    driver.get(url=url)
+    driver_wait(driver, By.CLASS_NAME, "container")
+    comment_tag=driver.find_elements(by=By.CLASS_NAME, value="container")[1]
+    comment_list = list(map(lambda x:x.text,comment_tag.find_elements(by=By.CLASS_NAME, value="comments-content")))
+    return comment_list
+
+def scrap_kado(url):
+    driver.get(url=url)
+    driver_wait(driver, By.CLASS_NAME, "reply-list")
+    comment_tag=driver.find_element(by=By.CLASS_NAME, value="reply-list")
+    comment_list = list(map(lambda x:x.text,comment_tag.find_elements(by=By.CLASS_NAME, value="comments-content")))
     return comment_list
 
 def scrap_hankyung(url):
@@ -396,7 +523,7 @@ def scrap_kbs(url):
                     more_btn.click()
                 except:
                     break
-            driver_wait(driver, By.CLASS_NAME, "reply-content")
+            # driver_wait(driver, By.CLASS_NAME, "reply-content")
 
             comment_list = list(map(lambda x:x.text,driver.find_elements(by=By.CLASS_NAME, value="reply-content")))
             driver.switch_to.default_content()
@@ -414,6 +541,43 @@ def scrap_sbs(url):
     iframe_list = driver.find_elements(by=By.TAG_NAME, value="iframe")
     for i in iframe_list:
         # print(i.get_attribute("src"))
+        if "라이브리 - 댓글영역" in i.get_attribute("title") or "�쇱씠釉뚮━ - �볤��곸뿭" in i.get_attribute("title"):
+            driver.switch_to.frame(i)
+            while(1):
+                try:
+                    time.sleep(1)
+                    more_btn=driver.find_element(by=By.CLASS_NAME, value="more-btn")
+                    more_btn.click()
+                except:
+                    break
+            # driver_wait(driver, By.CLASS_NAME, "reply-content")
+
+            comment_list = list(map(lambda x:x.text,driver.find_elements(by=By.CLASS_NAME, value="reply-content")))
+            driver.switch_to.default_content()
+            break
+
+    return comment_list
+
+def scrap_korea(url):
+    driver.get(url=url)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    for i in range(5):
+        time.sleep(1)
+        try:
+            more_btn = driver.find_element(by=By.CLASS_NAME, value="livere_comment").find_element(by=By.TAG_NAME,value="button")
+            more_btn.click()
+            break
+        except:
+            continue
+
+    find_iframe_name(driver,"라이브리 - 댓글영역",10)
+
+    comment_list=[]
+
+    iframe_list = driver.find_elements(by=By.TAG_NAME, value="iframe")
+    for i in iframe_list:
+        # print(i.get_attribute("src"))
         if "라이브리 - 댓글영역" in i.get_attribute("title"):
             driver.switch_to.frame(i)
             while(1):
@@ -423,7 +587,7 @@ def scrap_sbs(url):
                     more_btn.click()
                 except:
                     break
-            driver_wait(driver, By.CLASS_NAME, "reply-content")
+            # driver_wait(driver, By.CLASS_NAME, "reply-content")
 
             comment_list = list(map(lambda x:x.text,driver.find_elements(by=By.CLASS_NAME, value="reply-content")))
             driver.switch_to.default_content()
@@ -450,7 +614,7 @@ def scrap_hani(url):
                     more_btn.click()
                 except:
                     break
-            driver_wait(driver, By.CLASS_NAME, "reply-content")
+            # driver_wait(driver, By.CLASS_NAME, "reply-content")
 
             comment_list = list(map(lambda x:x.text,driver.find_elements(by=By.CLASS_NAME, value="reply-content")))
             driver.switch_to.default_content()
@@ -481,12 +645,62 @@ def scrap_seoul(url):
                     more_btn.click()
                 except:
                     break
-            driver_wait(driver, By.CLASS_NAME, "reply-content")
+            # driver_wait(driver, By.CLASS_NAME, "reply-content")
 
             comment_list = list(map(lambda x:x.text,driver.find_elements(by=By.CLASS_NAME, value="reply-content")))
             driver.switch_to.default_content()
 
     return comment_list
+
+def scrap_sedaily(url):
+    driver.get(url=url)
+    time.sleep(1)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight*0.6);")
+
+    driver_wait(driver, By.NAME, "realTalk")
+    driver.switch_to.frame(driver.find_element(by=By.NAME, value="realTalk"))
+
+    time.sleep(1)
+    try:
+        driver_wait(driver, By.CLASS_NAME, "btn-outline-more")
+        more_btn = driver.find_element(by=By.CLASS_NAME, value="btn-outline-more")
+        more_btn.click()
+    except:
+        pass
+
+    temp=0
+    conf=0
+    for i in range(10):
+        print(temp)
+        try:
+            now = len(driver.find_elements(by=By.CLASS_NAME, value="close"))
+        except:
+            time.sleep(1)
+            continue
+        if now==temp:
+            conf+=1
+        else:
+            temp=now
+            conf=0
+        if conf>3:
+            break
+
+    more_btn = driver.find_elements(by=By.CLASS_NAME, value="close")
+
+    for i in more_btn:
+        try:
+            print(i.find_element(by=By.CLASS_NAME, value="txt").text)
+            if "답글" in i.find_element(by=By.CLASS_NAME, value="txt").text:
+                i.click()
+                time.sleep(1)
+        except:
+            continue
+
+    comment_list = list(map(lambda x:x.text,driver.find_elements(by=By.CLASS_NAME, value="message")))
+    return comment_list
+
+
+
 
 def scrap_mk(url):
     driver.get(url=url)
@@ -557,4 +771,22 @@ def scrap_naver(url):
 # print(get_comment("http://www.gnnews.co.kr/news/articleView.html?idxno=527873"))
 # print(get_comment("https://www.hani.co.kr/arti/society/society_general/1089816.html"))
 # print(get_comment("https://www.hankyung.com/realestate/article/2023042814301?rss=r"))
-print(get_comment("https://www.hankyung.com/realestate/article/2023042814301?rss=r"))
+# print(get_comment("https://www.idomin.com/news/articleView.html?idxno=823988"))
+# print(get_comment("https://www.inven.co.kr/board/webzine/2097/2064411?iskin=webzine"))
+# print(get_comment("http://www.jemin.com/news/articleView.html?idxno=754471"))
+# print(get_comment("https://www.jeonmae.co.kr/news/articleView.html?idxno=956230"))
+# print(get_comment("https://www.jjan.kr/article/20230509580322"))
+# print(get_comment("https://www.joongang.co.kr/article/25159017#home"))
+# print(get_comment("http://www.joongboo.com/news/articleView.html?idxno=363590192"))
+# print(get_comment("http://www.kado.net/news/articleView.html?idxno=1152872"))
+# print(get_comment("https://www.khan.co.kr/politics/assembly/article/202305012110005/?utm_source=khan_rss&utm_medium=rss&utm_campaign=total_news"))
+# print(get_comment("https://www.korea.kr/news/policyNewsView.do?newsId=148914671&pWise=mostViewNewsSub&pWiseSub=B1"))
+# print(get_comment("http://www.kwnews.co.kr/page/view/2023051008233946461"))
+# print(get_comment("https://www.mk.co.kr/news/stock/10723775"))
+# print(get_comment("https://www.munhwa.com/news/view.html?no=2023043001039930295003"))
+# print(get_comment("https://www.ppomppu.co.kr/zboard/view.php?id=freeboard&no=8305871"))
+# print(get_comment("https://www.sedaily.com/NewsView/29OI5L5T7Y"))
+# print(get_comment("https://www.seoul.co.kr/news/newsView.php?id=20230501500162"))
+# print(get_comment("http://www.slrclub.com/bbs/vx2.php?id=hot_article&no=1077506"))
+# print(get_comment("https://www.todayhumor.co.kr/board/view.php?table=humorbest&no=1728922&s_no=1728922"))
+print(get_comment("https://www.yeongnam.com/web/view.php?key=20220914010001784"))
